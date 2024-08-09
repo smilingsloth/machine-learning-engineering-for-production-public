ef8a5b8cee518eaaf6b01373159e896aed1690be
Lars <smilingsloth90@gmail.com>
2024-08-09 23:14:19 +0200

    testing gh actions

--- course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/main.py
+++ course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/main.py
@@ -16,7 +16,7 @@ with open("models/wine.pkl", "rb") as file:
 class Wine(BaseModel):
     batches: List[conlist(item_type=float, min_length=13, max_length=13)]
 
-
+# a comment to trigger github actions
 @app.post("/predict")
 def predict(wine: Wine):
     batches = wine.batches
